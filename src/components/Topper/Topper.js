17b6eaf72a1a3371fa7576adac9d84a016430382
lizahalykina <lizavetahalykina@gmail.com>
2023-10-20 20:25:50 +0100

    lot of changes

--- src/components/Topper/Topper.js
+++ src/components/Topper/Topper.js
@@ -1,29 +1,34 @@
 import "./Topper.css";
 
-function Topper({ footerRef }) {
+function Topper({ footerRef, menuRef, coffeeRef }) {
     const scrollToFooter = () => {
         footerRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+    const scrollToMenu = () => {
+        menuRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+    const scrollToDrinkSlider = () => {
+        coffeeRef.current.scrollIntoView({ behavior: "smooth" });
     };
     return (
         <div className="Wrapper">
             <div className="Topper">
-                <div>
-                    <h1>Good coffee lives here</h1>
+                <div className="Logo">
+                    <img src={"/MockAssets/LogoTopperWeb.svg"} alt="Logo" />
                 </div>
-                <img
-                    className="Logo"
-                    src={"/MockAssets/TopperLogo.svg"}
-                    alt="Logo"
-                />
-                <div>
-                    <h2 onClick={scrollToFooter}>Where to find us</h2>
+                <div className="topText">
+                    <div>
+                        <h1 onClick={scrollToDrinkSlider}>Coffee</h1>
+                    </div>
+                    <div>
+                        <h2 onClick={scrollToMenu}>Menu</h2>
+                    </div>
+                    <div>
+                        <h3 onClick={scrollToFooter}>Contact us</h3>
+                    </div>
                 </div>
             </div>
-            <img
-                className="Bean"
-                src={"/MockAssets/CoffeeBeanPath.svg"}
-                alt="Bean"
-            />
+            <img className="Bean" src={"/MockAssets/BeanLong.svg"} alt="Bean" />
         </div>
     );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -7,12 +7,18 @@ import "./Main.css";
 
 function Main() {
     const footerRef = useRef(null);
+    const menuRef = useRef(null);
+    const coffeeRef = useRef(null);
     return (
         <div className="Main">
-            <Topper footerRef={footerRef} />
-            <DrinkSlider />
-            <Menu />
-            <Footer footerRef={footerRef} />
+            <Topper
+                footerRef={footerRef}
+                menuRef={menuRef}
+                coffeeRef={coffeeRef}
+            />
+            <DrinkSlider coffeeRef={coffeeRef} />
+            <Menu className="MenuMain" menuRef={menuRef} />
+            <Footer className="FooterMain" footerRef={footerRef} />
         </div>
     );
 }
